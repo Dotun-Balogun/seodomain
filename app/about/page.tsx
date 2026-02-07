12da45f9ecd6403135beea51d2286cf015ee18de
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-02-07 23:37:12 +0100

    about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <section>
+        <h1 className='text-2xl md:text-4xl font-bold '>About Us</h1>
+        <p className='mt-4 text-gray-600'>We are a company dedicated to providing the best services to our customers.</p>
+    </section>
   )
 }
 
