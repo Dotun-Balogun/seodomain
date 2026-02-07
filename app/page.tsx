0755ce0970bf52cfc72c3ec10cd1f419c7c69282
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-02-07 23:11:03 +0100

    homepage correction

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <main className='w-full h-full flex flex-col items-center justify-center'>
-    <section className='w-full min-h-screen max-w-7xl mx-auto px-4 bg-amber-300 flex flex-col items-center justify-center gap-4'>
+    <section className='w-full min-h-[50vh] lg:min-h-screen max-w-7xl mx-auto px-4 flex flex-col items-center justify-center gap-4'>
          <h1 className='text-2xl md:text-4xl font-bold '>Welcome to the Homepage</h1>
          <p>
            This is the HomePage
