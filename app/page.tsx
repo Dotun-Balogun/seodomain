bd5ebea7b5c0782540380ea03d004512e848f1a5
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-02-07 22:42:50 +0100

    homepage section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <main className='w-full h-full flex flex-col items-center justify-center'>
+    <section className='w-full min-h-screen max-w-7xl mx-auto px-4 bg-amber-300 flex flex-col items-center justify-center gap-4'>
+         <h1 className='text-2xl md:text-4xl font-bold '>Welcome to the Homepage</h1>
+         <p>
+           This is the HomePage
+         </p>
+    </section>
+
+    </main>
   )
 }
 
