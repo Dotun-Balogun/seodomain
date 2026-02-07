56cfe2d1a8c8d46037595c534cb2529a18167d91
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2026-02-07 23:32:51 +0100

    footer

---
+++ components/ui/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-full bg-gray-800 text-white py-4 mt-auto'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <p className='text-center'>© {new Date().getFullYear()} MyApp. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
